05863b4d88eeb166adc8ad25ebdd10eb9b1ceb75
ARAFAT O. OLAYIWOLA <Harof.dev@gmail.com>
2022-08-05 13:23:52 +0100

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,7 +1,7 @@
 
 import time
 
-# fibonacci with dynaimc programming
+# fibonacci with dynaimc programming(memoization)
 def fib_cache(n=50, cache=None):
   
     if cache == None:
@@ -22,3 +22,16 @@ def fib_cache(n=50, cache=None):
     return result
 
 print(fib_cache())
+
+    
+  
+ #dynamic programming method(bottom-up)
+def fibonacci_btm_up(n=10):
+    result_so_far = [0, 1]
+    
+    for i in range(2, n+1):
+        print(result_so_far)
+        result_so_far.append(result_so_far[i-1] + result_so_far[i-2])
+    return result_so_far[n]
+    
+print(fibonacci_btm_up())
