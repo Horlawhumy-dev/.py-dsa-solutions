28c92a99a4af8ecdb89151533cedf6a16c447043
ARAFAT O. OLAYIWOLA <Harof.dev@gmail.com>
2022-08-06 13:27:39 +0100

    Update ransome_note.py

--- ransome_note.py
+++ ransome_note.py
@@ -6,13 +6,20 @@ def ransome_note(magazine, note):
         @params: magazine contains all words, note has all words to check for
         @return: True or False
     """
-    
-    mag_list = magazine.split(" ")
-    note_list = note.split(" ")
-    mag_dict = {mag_word: "stored" for mag_word in mag_list}
-    for note_word in note_list:
-        if note_word not in mag_dict:
-            return False
-    return True
+    mag_dict = {}
+        
+        for i in range(len(magazine)):
+            if magazine[i] in mag_dict:
+                mag_dict[magazine[i]] += 1 
+            else:
+                mag_dict[magazine[i]] = 1
+
+        for i in range(len(ransomNote)):
+            if mag_dict.get(ransomNote[i], 0) < 1:
+                return False
+            else:
+                mag_dict[ransomNote[i]] -= 1
+            
+        return True
     
 print(ransome_note("The man was captured by bandit", "The man was captured yesterday"))
